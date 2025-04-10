8870bc24a258d90e37240848134d2e882fc8ec4e
diamond-service <diamond.service.livorno@gmail.com>
2025-04-10 21:41:56 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
+import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig({ plugins: [react()], base: './' });
+=======
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './'
 });
+>>>>>>> 73d3f95a564fccc036d1acbdc36f1ed625e2bc64
