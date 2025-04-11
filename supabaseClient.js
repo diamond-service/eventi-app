447332765feea03f4355a039b137acf85d7a6cff
diamond-service <diamond.service.livorno@gmail.com>
2025-04-11 20:42:08 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-import { createClient } from '@supabase/supabase-js'; const supabaseUrl = 'https://xfyovnzmcrqpixblbfqc.supabase.co'; const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmeW92bnptY3JxcGl4YmxiZnFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3ODY2MDQsImV4cCI6MjA1OTM2MjYwNH0.r0But5RrqtYXB5mxvkNUxWiXE8edG9yVOybQRV-FORA'; export const supabase = createClient(supabaseUrl, supabaseKey);
-=======
+// supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://xfyovnzmcrqpixblbfqc.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmeW92bnptY3JxcGl4YmxiZnFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3ODY2MDQsImV4cCI6MjA1OTM2MjYwNH0.r0But5RrqtYXB5mxvkNUxWiXE8edG9yVOybQRV-FORA';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzIiwicm...'; // Chiave pubblica
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
->>>>>>> 8870bc24a258d90e37240848134d2e882fc8ec4e
