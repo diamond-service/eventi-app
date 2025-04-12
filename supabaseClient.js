1ea1052baed90aad22e6d510c75c45b189c82154
diamond-service <diamond.service.livorno@gmail.com>
2025-04-12 10:13:20 +0200

    Add files via upload

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://xfyovnzmcrqpixblbfqc.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmeW92bnptY3JxcGl4YmxiZnFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3ODY2MDQsImV4cCI6MjA1OTM2MjYwNH0.r0But5RrqtYXB5mxvkNUxWiXE8edG9yVOybQRV-FORA'; // Chiave pubblica
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzIiwicm...'; // Chiave pubblica
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
