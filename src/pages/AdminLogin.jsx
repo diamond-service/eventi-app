4c8730bf912f8860afa0c17d3e35a5cd91578424
diamond-service <diamond.service.livorno@gmail.com>
2025-04-12 11:54:14 +0200

    Update AdminLogin.jsx

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../supabaseClient.js';
 import { useNavigate } from 'react-router-dom';
 
 export default function AdminLogin() {
