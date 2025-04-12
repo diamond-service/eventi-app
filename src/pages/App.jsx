55b975077bf60e9ef447a2f31547c0b30f573426
diamond-service <diamond.service.livorno@gmail.com>
2025-04-12 12:36:01 +0200

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import BottomNav from '../components/BottomNav';
 
 export default function App() {
-  const [events, setEvents] = useState([]);
+ //  const [events, setEvents] = useState([]);
 
  // useEffect(() => {
  //   const fetchEvents = async () => {
